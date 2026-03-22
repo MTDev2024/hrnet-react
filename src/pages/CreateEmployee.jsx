e2bcc9fe5949a620b2923dc6adea86ff106ed9f0
Michael <mt.dev.2023@gmail.com>
2026-03-23 00:14:39 +0100

    feat: add CreateEmployee form structure

--- src/pages/CreateEmployee.jsx
+++ src/pages/CreateEmployee.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+
 function CreateEmployee() {
-  return <h1>Create Employee</h1>;
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    dateOfBirth: "",
+    startDate: "",
+    street: "",
+    city: "",
+    state: "",
+    zipCode: "",
+    department: "",
+  });
+
+  return (
+    <>
+      <h1>Create Employee</h1>;
+      <label>
+        First Name: <input name="firstName" />
+      </label>
+      <label>
+        Last Name: <input name="lastName" />
+      </label>
+      <label>
+        Street: <input name="street" />
+      </label>
+      <label>
+        City: <input name="city" />
+      </label>
+      <label>
+        Zip Code: <input name="zipCode" />
+      </label>
+    </>
+  );
 }
 
 export default CreateEmployee;
