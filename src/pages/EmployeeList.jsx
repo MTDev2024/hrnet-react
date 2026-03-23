f92781e24ae2f16908a8806246a9353734fb0046
MTDev2024 <mt.dev.2023@gmail.com>
2026-03-23 03:03:19 +0100

    feat: add EmployeeList with TanStack Table

--- src/pages/EmployeeList.jsx
+++ src/pages/EmployeeList.jsx
@@ -1,5 +1,64 @@
+import {
+  getCoreRowModel,
+  useReactTable,
+  createColumnHelper,
+  flexRender,
+} from "@tanstack/react-table";
+import { useEmployeeStore } from "../store/employeeStore";
+
+const columnHelper = createColumnHelper();
+
 function EmployeeList() {
-  return <h1>Employee List</h1>;
+  const employees = useEmployeeStore((state) => state.employees);
+  const columns = [
+    columnHelper.accessor("firstName", { header: "First Name" }),
+    columnHelper.accessor("lastName", { header: "Last Name" }),
+    columnHelper.accessor("dateOfBirth", { header: "Date of Birth" }),
+    columnHelper.accessor("startDate", { header: "Start Date" }),
+    columnHelper.accessor("street", { header: "Street" }),
+    columnHelper.accessor("city", { header: "City" }),
+    columnHelper.accessor("state", { header: "State" }),
+    columnHelper.accessor("zipCode", { header: "Zip Code" }),
+    columnHelper.accessor("department", { header: "Department" }),
+  ];
+
+  const table = useReactTable({
+    data: employees,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  console.log(employees);
+
+  return (
+    <table>
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id}>
+                {flexRender(
+                  header.column.columnDef.header,
+                  header.getContext(),
+                )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export default EmployeeList;
